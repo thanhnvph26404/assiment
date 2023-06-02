369e3cdee49c03ba7bd664458161cd5e72907315
thanhnvph26404 <thanhnvph26404@gmail.com>
2023-06-02 10:40:30 +0700

    style: create ui MenuPage refs: AS-4

--- server/src/app.js
+++ server/src/app.js
@@ -3,17 +3,13 @@ import express from 'express'
 import morgan from 'morgan'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import categoriesRouter from './routers/categories'
-import productsRouter from './routers/products'
-import upload from './routers/upload'
+
 const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 
-app.use('/api', categoriesRouter)
-app.use('/api', productsRouter)
-app.use('/api', upload)
+
 mongoose.connect(process.env.MONGO_URI)
 export const viteNodeApp = app
